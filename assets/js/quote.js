a91cc176902ce054b354ae80dcafd6040fe6574d
N/A <theprophetofregret@protonmail.com>
2021-10-04 11:13:02 -0400

    I don't care what you think this formatting is a million times better

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,81 +1,106 @@
-$(document).ready(function () {
+$(document).ready(function () 
+{
     $("#numElev_2, #numElev_3, #elevPriceUnit, #elevTotal, #installationFee, #total_").attr('readonly', true);
 
     var numApp, numFloors, numBase, maxOcc;
-    var prodRange = {
+    var prodRange = 
+    {
         type: null,
         price: null,
         installationFeePercentage: null,
     }
 
-    $('.formField').on('keyup', function () {
+    $('.formField').on('keyup', function () 
+    {
         doCalc();
     })
 
 
     //At least it's more specific and fixes the problem where only the standard price is displayed! But it's hideous.
-    $('#standard').on('click', function () {
+    $('#standard').on('click', function () 
+    {
         document.getElementById('elevPriceUnit').value = (7565).toFixed(2) + " $";
         doCalc();
     })
-    $('#premium').on('click', function () {
+
+    $('#premium').on('click', function () 
+    {
         document.getElementById('elevPriceUnit').value = (12345).toFixed(2) + " $";
         doCalc();
     })
-    $('#excelium').on('click', function () {
+
+    $('#excelium').on('click', function () 
+    {
         document.getElementById('elevPriceUnit').value = (15400).toFixed(2) + " $";
         doCalc();
     })
 
-    $('#residential, #commercial, #corporate, #hybrid').on('click', function () {
+    $('#residential, #commercial, #corporate, #hybrid').on('click', function ()
+    {
         initialize();
     })
 
 
-    function initialize() {
+    function initialize() 
+    {
         $('.formField').val('');
         $('.productRangeBtn').prop('checked', false);
     }
-
-    function getInfoNumApp() {
+    //Appartments
+    function getInfoNumApp() 
+    {
         numApp = $('#numApp').val();
     }
-
-    function getInfoNumFloors() {
+    //Floors
+    function getInfoNumFloors() 
+    {
         numFloors = $('#numFloors').val();
     }
-
-    function getInfoNumBase() {
+    //Basements
+    function getInfoNumBase() 
+    {
         numBase = $('#numBase').val();
     }
-
-    function getInfoNumElev() {
+    //Elevator shafts
+    function getInfoNumElev() 
+    {
         numElev = $('#numElev').val();
     }
-
-    function getInfoMaxOcc() {
+    //Occupancy
+    function getInfoMaxOcc() 
+    {
         maxOcc = $('#maxOcc').val();
     }
 
-    function getProdRange() {
-        if ($('#standard').is(':checked')) {
+    //Product range
+    function getProdRange() 
+    {
+        if ($('#standard').is(':checked')) 
+        
+        {
             prodRange.type = "standard";
             prodRange.price = parseFloat(7565);
             prodRange.installationFeePercentage = 0.1;
             return prodRange;
 
-        } else if ($('#premium').is(':checked')) {
+        } else if ($('#premium').is(':checked')) 
+        
+        {
             prodRange.type = "premium";
             prodRange.price = parseFloat(12345);
             prodRange.installationFeePercentage = 0.13;
             return prodRange;
 
-        } else if ($('#excelium').is(':checked')) {
+        } else if ($('#excelium').is(':checked')) 
+        
+        {
             prodRange.type = "excelium";
             prodRange.price = parseFloat(15400);
             prodRange.installationFeePercentage = 0.16;
             return prodRange;
-        } else {
+        } else 
+        
+        {
             prodRange.type = null;
             prodRange.price = null;
             prodRange.installationFeePercentage = null;
@@ -84,7 +109,8 @@ $(document).ready(function () {
     }
 
     //This function seems to be the one giving the numbers to the final result fields. More tracking needed to fully understand.
-    function GetInfos() {
+    function GetInfos() 
+    {
         getInfoNumApp();
         getInfoNumFloors();
         getInfoNumBase();
@@ -94,24 +120,28 @@ $(document).ready(function () {
     }
 
     //NaN fault by specifying it is a Number
-    function setRequiredElevatorsResult(finNumElev) {
-        $("#numElev_1, #numElev_2, #numElev_3").val(parseFloat(Number(finNumElev)));
+    function setRequiredElevatorsResult(finNumElev) 
+    {
+        $("#numElev_2, #numElev_3").val(parseFloat(Number(finNumElev)));
     }
 
     //NaN faults fixed by specifying that it is a Number
-    function setPricesResults(finNumElev, roughTotal, installFee, total) {
+    function setPricesResults(roughTotal, installFee, total) 
+    {
         $("#elevTotal").val(parseFloat(Number(roughTotal)).toFixed(2) + " $"); 
         $("#installationFee").val(parseFloat(Number(installFee)).toFixed(2) + " $");
         $("#total_").val(parseFloat(Number(total)).toFixed(2) + " $");
     }
 
-    function emptyElevatorsNumberAndPricesFields() {
+    function emptyElevatorsNumberAndPricesFields() 
+    {
         $('#numElev_3').val('');
         $('.priceField').val('');
         $("#numElev_2").val("");
     }
 
-    function createFormData(projectType) {
+    function createFormData(projectType) 
+    {
         return {
             numberApp: numApp,
             numberFloors: numFloors,
@@ -122,7 +152,8 @@ $(document).ready(function () {
         }
     }
     //If you arrange {blocks of codes like these} in this way it is easier to determine where to put additional code.
-    function negativeValues() {
+    function negativeValues() 
+    {
         if ($('#numApp').val() < 0) 
         {
             alert("Please enter a positive number!");
@@ -185,23 +216,27 @@ $(document).ready(function () {
         }
     }
 
-    function apiCall(projectType) {
+    function apiCall(projectType) 
+    {
         //Getting numbers from quote
         GetInfos();
 
         //Preparing data for Api call
         formData = createFormData(projectType)
 
-        $.ajax({
+        $.ajax(
+        {
             type: "POST",
             // url: 'http://localhost:3000/api/quoteCalculation/', //for local testing
             url: 'https://rocketelevators-quote.herokuapp.com/api/quoteCalculation/',
             data: JSON.stringify(formData),
             contentType: "application/json; charset=utf-8",
             dataType: "json",
-            success: function (data) {
+            success: function (data) 
+            {
                 setRequiredElevatorsResult(data.finalNumElev);
-                if (prodRange.type != null) {
+                if (prodRange.type != null) 
+                {
                     setPricesResults(data.finalNumElev, data.subTotal, data.installationFee, data.grandTotal);
                 }
             }
@@ -209,7 +244,8 @@ $(document).ready(function () {
     }
     
     //more readable this way.
-    function doCalc() {
+    function doCalc() 
+    {
         if ($('#residential').hasClass('active') && 
         !negativeValues() && 
         $('#numApp').val() &&
